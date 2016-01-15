88df4ff39cb0cd2becea2feaa5c41b0408c8e21e
navdeep-G <mr.navdeepgill@gmail.com>
2016-01-15 13:02:00 -0800

    tryCatch() in quadratic function

---
+++ BeginningPython/DecisionStructs/quadratic5.py
@@ -0,0 +1,15 @@
+# quadratic5.py
+import math 
+
+def main():
+    print ("This program finds the real solutions to a quadratic\n")
+
+    try:
+        a, b, c = eval(input("Please enter the coefficients (a, b, c): "))
+        discRoot = math.sqrt(b * b - 4 * a * c)
+        root1 = (-b + discRoot) / (2 * a)
+        root2 = (-b - discRoot) / (2 * a)
+        print("\nThe solutions are:", root1, root2)
+    except ValueError:
+        print("\nNo real roots")
+main()
